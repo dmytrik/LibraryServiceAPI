7d9c18bc6cf4636b0078cd937563d722f0309339
Bondzik-S <bondziks@gmail.com>
2025-01-02 21:13:46 +0200

    feat: fix validatation

--- book/serializers.py
+++ book/serializers.py
@@ -5,18 +5,31 @@ from book.models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     """
-        Serializer for the Book model with validation for inventory and daily_fee.
+    Serializer for the Book model with validation for inventory and daily_fee.
     """
+
+    unreturned_borrowings_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Book
-        fields = ("id", "title", "author", "cover", "inventory", "daily_fee",)
+        fields = (
+            "id",
+            "title",
+            "author",
+            "cover",
+            "inventory",
+            "daily_fee",
+            "unreturned_borrowings_count",
+        )
+        read_only_field = ("id",)
 
     def validate(self, data):
-        if data['inventory'] < 0:
-            raise serializers.ValidationError({"inventory": "Inventory cannot be negative."})
-
-        if data['daily_fee'] <= 0:
-            raise serializers.ValidationError({"daily_fee": "Daily fee must be greater than zero."})
+        if data["daily_fee"] <= 0:
+            raise serializers.ValidationError(
+                {"daily_fee": "Daily fee must be greater than zero."}
+            )
 
         return data
+
+    def get_unreturned_borrowings_count(self, obj):
+        return obj.borrowings.filter(actual_return_date__isnull=True).count()
