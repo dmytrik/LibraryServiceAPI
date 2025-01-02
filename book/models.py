06ab9efe759db0b5ebc49c988efe46043f15dbad
Bondzik-S <bondziks@gmail.com>
2025-01-02 21:13:02 +0200

    feat: add docstring and str method

--- book/models.py
+++ book/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 
 class Book(models.Model):
+    """
+    Represents a book in the library with attributes like title,
+    author, cover type, inventory, and daily fee.
+    """
+
     class Cover(models.TextChoices):
-        HARD = "HARD",
-        SOFT = "SOFT",
+        HARD = ("HARD",)
+        SOFT = ("SOFT",)
 
     title = models.CharField(max_length=63, unique=True)
     author = models.CharField(max_length=63)
@@ -15,3 +20,6 @@ class Book(models.Model):
     )
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
