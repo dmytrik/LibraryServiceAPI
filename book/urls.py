2126012536f13c153f215e04ae99fa17b67a1d3e
Bondzik-S <bondziks@gmail.com>
2025-01-03 11:06:42 +0200

    feat: changed links

--- book/urls.py
+++ book/urls.py
@@ -5,6 +5,6 @@ from book.views import BookViewSet
 app_name = "book"
 
 router = routers.DefaultRouter()
-router.register("books", BookViewSet)
+router.register("", BookViewSet, basename="books")
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = router.urls
--- core/urls.py
+++ core/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path("api/book-service/", include("book.urls", namespace="book")),
+    path("api/books/", include("book.urls", namespace="book")),
 ]
